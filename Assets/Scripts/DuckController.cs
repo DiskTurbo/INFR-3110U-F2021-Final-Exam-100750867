339b36c80a6fe91f013e6119349f94f010e4e4b0
Robbo Bobbo <ryuhoshi101@gmail.com>
2021-12-17 18:23:02 -0500

    Initial commit

---
+++ Assets/Scripts/DuckController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DuckController : MonoBehaviour
+{
+    public static DuckController instance { get; private set; }
+    public float xSpeed, ySpeed = 0f;
+    public float xSpeedRand = 0.25f;
+    public bool isAlive = true;
+    Sprite spriteRend;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    [DllImport("Boss-Mode")]
+    private static extern void SetBossStats(float x, float y);
+    void Start()
+    {
+        SetBossStats(this.transform.localScale.x, this.transform.localScale.y);
+        spriteRend = GetComponent<Sprite>();
+        xSpeed = xSpeed + Random.Range(-xSpeedRand, xSpeedRand);
+        if(xSpeed >= 0)
+        {
+            SpriteFlipX(false);
+        }
+        else
+        {
+            SpriteFlipX(true);
+        }
+        StartCoroutine(DirectionChange(Random.Range(2.5f, 4f)));
+        StartCoroutine(DisappearAfterTime(10f));
+    }
+
+    private void LateUpdate()
+    {
+        transform.Translate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, 0f);
+    }
+
+    public void setDirection(float dir)
+    {
+        xSpeed *= dir;
+    }
+
+    void SpriteFlipX(bool flip)
+    {
+        if(flip)
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x * -1f, this.transform.localScale.y, this.transform.localScale.z);
+        }  
+        else
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x * 1f, this.transform.localScale.y, this.transform.localScale.z);
+        }
+    }
+
+    void SpriteFlipY(bool flip)
+    {
+        if (flip)
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y * -1f, this.transform.localScale.z);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y * 1f, this.transform.localScale.z);
+        }
+    }
+
+    public void Down()
+    {
+        isAlive = false;
+        SpriteFlipY(true);
+        Destroy(this.GetComponent<Collider2D>());   
+        ySpeed = -300f;
+        xSpeed = 0f;
+        StartCoroutine(DisappearAfterTime(0.8f));
+    }
+    public IEnumerator DisappearAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+    public void Disappear()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator DirectionChange(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if(isAlive == true)
+        {
+            ySpeed = Mathf.Abs(xSpeed);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ySpeed = -ySpeed;
+        xSpeed = -xSpeed + Random.Range(-xSpeedRand, xSpeedRand);
+        SpriteFlipX(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject CRTFLASH;
+    [SerializeField] GameObject BG, UI, DuckSpawner, duckPrefab, Pause;
+    public int ammo = 3;
+    public bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && isPaused == false)
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+            Pause.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) && isPaused == true)
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+            Pause.SetActive(false);
+        }
+        if(Input.GetMouseButtonDown(0) && DuckController.instance == true && ammo != 0)
+        {
+            StartCoroutine(LightGunFlash(0.02f));
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                DuckController.instance.Down();
+            }
+            ammo--;
+        }
+        if (ammo == 0 && DuckController.instance == true)
+        {
+            DuckController.instance.StartCoroutine(DuckController.instance.DisappearAfterTime(1f));
+            StartCoroutine(SpawnDuck(1f));
+        }
+    }
+
+    IEnumerator SpawnDuck(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(duckPrefab, DuckSpawner.transform);
+    }
+
+    IEnumerator LightGunFlash(float time)
+    {
+        CRTFLASH.SetActive(true);
+        DuckController.instance.transform.GetChild(0).gameObject.SetActive(true);
+        UI.SetActive(false);
+        BG.SetActive(false);
+        yield return new WaitForSeconds(time);
+        DuckController.instance.transform.GetChild(0).gameObject.SetActive(false);
+        CRTFLASH.SetActive(false);
+        UI.SetActive(true);
+        BG.SetActive(true);
+    }
+}
